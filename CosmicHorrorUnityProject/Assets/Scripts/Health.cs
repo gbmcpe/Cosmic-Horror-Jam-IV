87eb74803e3633c67ba2d192b5b068c826e17e76
gbmcpe <gbmcpe1@gmail.com>
2025-10-20 19:45:15 -0500

    Revert "Scoring + Death to main menu"
    This reverts commit 78b6ad28f5ccce4cde6eca4f798bbe5c312ba349.

--- CosmicHorrorUnityProject/Assets/Scripts/Health.cs
+++ CosmicHorrorUnityProject/Assets/Scripts/Health.cs
@@ -1,22 +1,14 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-
 
 public class Health : MonoBehaviour
 {
     public int health;
     public int maxHP = 3;
-    public bool invuln = false;
-    public Sprite Broken;
-    
-    
-    [SerializeField] private Image health1;
-    [SerializeField] private Image health2;
-    [SerializeField] private Image health3;
-    [SerializeField] private CanvasGroup Blackout;
+    [SerializeField] private GameObject health1;
+    [SerializeField] private GameObject health2;
+    [SerializeField] private GameObject health3;
     void Start()
     {
         health = maxHP;
@@ -26,40 +18,21 @@ public class Health : MonoBehaviour
 
     }
 
-/*public void blackout()
-    {
-        for (int i = 0; i < 100; i++)
-        {
-            Blackout.alpha += 0.01f;  Currently breaks, idk man
-        }
-    } 
-    */
     public void TakeDamage(int Damage)
     {
-        if (invuln == false)
-        {
-            StartCoroutine(Invulntime());
-            health -= Damage;
-            if (health <= 0)
-            {
-                health1.sprite = Broken;
-                // blackout();
-                SceneManager.LoadScene("Main Menu");
-            }
-            if (health == 2)
-            {
-                health3.sprite = Broken;
-            }
-            if (health == 1)
-            {
-                health2.sprite = Broken;
-            }
+        health -= Damage;
+        if (health <= 0)
+        {   
+            health1.SetActive(false);
+            Destroy(gameObject);
         }
-    }
-    IEnumerator Invulntime()
-    {
-        invuln = true;
-        yield return new WaitForSeconds(1f);
-        invuln = false;
+        if (health == 2)
+        {
+            health3.SetActive(false);
+        }
+        if (health == 1)
+        {
+            health2.SetActive(false);
+        }
     }
 }
--- CosmicHorrorUnityProject/Assets/Scripts/ScoringSystem.cs
+++ CosmicHorrorUnityProject/Assets/Scripts/ScoringSystem.cs
@@ -4,8 +4,7 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class ScoringSystem : MonoBehaviour
-{   
-    [SerializeField] public int health;
+{
     [SerializeField] private int Score;
     [SerializeField] private TextMeshProUGUI scoreText; // UI Text reference to display score
     
@@ -15,12 +14,8 @@ public class ScoringSystem : MonoBehaviour
         Score = 0;
         scoreText.text = Score.ToString();
         StartCoroutine(ScoreIncrement());
+    }
 
-    }
-    void Update()
-    {
-        health = GameObject.Find("Player").GetComponent<Health>().health;
-    }
     // More efficient coroutine approach
     IEnumerator ScoreIncrement()
     {
@@ -29,11 +24,6 @@ public class ScoringSystem : MonoBehaviour
             yield return new WaitForSeconds(1f); // Wait 1 second
             Score += 1;
             scoreText.text = Score.ToString();
-            if (health <= 0)
-            {
-                break;  // Break loop when health = 0, agony
-            }
         }
     }
-    
 }
